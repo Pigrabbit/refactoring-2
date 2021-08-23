e7aace672965f5bdb0827bf8b82eeb6f0021e3c4
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-08-23 22:18:37 +0900

    [Chapter 1] Add HTML version of statement

--- ch1/src/statement.after.js
+++ ch1/src/statement.after.js
@@ -16,12 +16,32 @@ function renderPlainText(data) {
   result += `Total: ${usd(data.totalAmount)}\n`;
   result += `Credits: ${data.totalVolumeCredits}p\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
+export function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement (Customer: ${data.customer})</h1>\n`;
+  result += `<table>\n`;
+  result += `<tr><th>play</th><th>audience</th><th>amount</th></tr>`;
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience} attendance)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
+
+  result += "</table>\n";
+  result += `<p>Total: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>Credits: <em>${data.totalVolumeCredits}</em>p</p>\n`;
+
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
