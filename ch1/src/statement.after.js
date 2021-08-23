876f406316d5b9b2eca743dc8482e0b46676c3ad
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-08-23 13:01:04 +0900

    [Chapter 1] Refactor the statement funciton by extracting method

---
+++ ch1/src/statement.after.js
@@ -0,0 +1,54 @@
+module.exports = function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement (Customer: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
+
+    // accumulate credits
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // add points for every 5 comedy audience
+    if (play.type === "comedy") volumeCredits += Math.floor(perf.audience / 5);
+
+    // prints statement
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    } attendance)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `Total: ${format(totalAmount / 100)}\n`;
+  result += `Credits: ${volumeCredits}p\n`;
+  return result;
+};
+
+function amountFor(perf, play) {
+  let thisAmount = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`Unknown Genre: ${play.type}`);
+  }
+
+  return thisAmount;
+}
--- ch1/src/statement.spec.js
+++ ch1/src/statement.spec.js
@@ -1,8 +1,10 @@
-const statement = require("./statement");
+const statementBefore = require("./statement.before");
+const statementAfter = require("./statement.after");
+
 const mockInvoices = require("./invoices.json");
 const mockPlays = require("./plays.json");
 
-describe("statement function", () => {
+describe("[before] statement function", () => {
   describe("invoice and play list is given", () => {
     it("generates statement with total amount and credits", () => {
       const expectedStatement = `Statement (Customer: BigCo)
@@ -13,7 +15,27 @@ Total: $1,730.00
 Credits: 47p
 `;
 
-      expect(statement(mockInvoices, mockPlays)).toEqual(expectedStatement);
+      expect(statementBefore(mockInvoices, mockPlays)).toEqual(
+        expectedStatement
+      );
+    });
+  });
+});
+
+describe("[after] statement function", () => {
+  describe("invoice and play list is given", () => {
+    it("generates statement with total amount and credits", () => {
+      const expectedStatement = `Statement (Customer: BigCo)
+ Hamlet: $650.00 (55 attendance)
+ As You Like It: $580.00 (35 attendance)
+ Othello: $500.00 (40 attendance)
+Total: $1,730.00
+Credits: 47p
+`;
+
+      expect(statementAfter(mockInvoices, mockPlays)).toEqual(
+        expectedStatement
+      );
     });
   });
 });
