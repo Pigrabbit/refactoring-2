c17cb2cbce3fb7c039db518c9237a27b36ce5304
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-08-23 21:58:34 +0900

    [Chapter 1] Extract createStatementDate method

--- ch1/src/statement.after.js
+++ ch1/src/statement.after.js
@@ -1,66 +1,10 @@
+import createStatementData from "./createStatementData";
+
 export default function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-
-  return renderPlainText(statementData, plays);
-
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
-
-  function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
-
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance); // shallow copy
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`Unknown Genre: ${aPerformance.play.type}`);
-    }
-
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-    if (aPerformance.play.type === "comedy")
-      volumeCredits += Math.floor(aPerformance.audience / 5);
-    return volumeCredits;
-  }
+  return renderPlainText(createStatementData(invoice, plays));
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `Statement (Customer: ${data.customer})\n`;
   for (let perf of data.performances) {
     // prints statement
