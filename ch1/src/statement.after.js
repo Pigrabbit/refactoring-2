006ba046169f8d4fd3fee366d27066dd767da229
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-08-23 21:54:37 +0900

    [Chapter 1] Replace Loop with Pipeline

--- ch1/src/statement.after.js
+++ ch1/src/statement.after.js
@@ -8,19 +8,11 @@ export default function statement(invoice, plays) {
   return renderPlainText(statementData, plays);
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function enrichPerformance(aPerformance) {
