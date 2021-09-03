4d2289d4a990e2a96a7bbd2b0eff2a340a52a679
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-03 12:10:05 +0900

    [Chapter 4] Extract fixture setup from it blocks

--- ch4/src/province.spec.js
+++ ch4/src/province.spec.js
@@ -2,13 +2,16 @@ import { Province } from "./province";
 import { sampleProvinceData } from "./sampleProvinceData";
 
 describe("Province", () => {
+  let asia;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
+
   it("shortfall", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toEqual(5);
   });
 
   it("profit", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toEqual(230);
   });
 });
