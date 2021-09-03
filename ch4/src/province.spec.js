c86f679fa331265f78bd987b53ce16fcb7288ef1
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-03 14:22:40 +0900

    [Chapter 4] Add test case for production setting method

--- ch4/src/province.spec.js
+++ ch4/src/province.spec.js
@@ -14,4 +14,10 @@ describe("Province", () => {
   it("profit", () => {
     expect(asia.profit).toEqual(230);
   });
+
+  it("change production", () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toEqual(-6);
+    expect(asia.profit).toEqual(292);
+  });
 });
