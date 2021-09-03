d7c1eddec52e3bbe74cbecadb1ed2914d039593a
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-03 11:45:56 +0900

    [Chapter 4]: Add sample code which needs test suite

---
+++ ch4/src/province.js
@@ -0,0 +1,67 @@
+import { Producer } from "./producer";
+export class Province {
+  constructor(doc) {
+    this._name = doc.name;
+    this._producers = doc.producers;
+    this._totalProduction = 0;
+    this._demand = doc.demand;
+    this._price = doc.price;
+    doc.producers.forEach((d) => this.addProducer(new Producer(this, d)));
+  }
+
+  addProducer(arg) {
+    this._producers.push(arg);
+    this._totalProduction += arg.production;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get producers() {
+    return this._producers;
+  }
+
+  get totalProduction() {
+    return this._totalProduction;
+  }
+  set totalProduction(arg) {
+    this._totalProduction = parseInt(arg); // parse to integer and save
+  }
+
+  get price() {
+    return this._price;
+  }
+  set price(arg) {
+    this._price = parseInt(arg); // parse to integer and save
+  }
+
+  get shortfall() {
+    return this._demand - this.totalProduction;
+  }
+
+  get profit() {
+    return this.demandValue - this.demandCost;
+  }
+
+  get demandValue() {
+    return this.satisfiedDemand * this.price;
+  }
+
+  get satisfiedDemand() {
+    return Math.min(this._demand, this.totalProduction);
+  }
+
+  get demandCost() {
+    let remainingDemand = this._demand;
+    let result = 0;
+    this.producers
+      .sort((a, b) => a.cost - b.cost)
+      .forEach((p) => {
+        const contribution = Math.min(remainingDemand, p.production);
+        remainingDemand -= contribution;
+        result += contribution * p.cost;
+      });
+    return result;
+  }
+}
