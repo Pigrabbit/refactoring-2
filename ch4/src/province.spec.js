f6ec47e6e64cb6cfd7e771667c5db5f7d8830939
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-03 12:09:03 +0900

    [Chapter 4] Add test suite for profit method

--- ch4/src/province.spec.js
+++ ch4/src/province.spec.js
@@ -6,4 +6,9 @@ describe("Province", () => {
     const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toEqual(5);
   });
+
+  it("profit", () => {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.profit).toEqual(230);
+  });
 });
