c740d8e41e3f190a160883cdf7c81cc8ba81c3e2
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-12 21:39:20 +0900

    [Chapter 6] Extract shipping variable

--- ch6/src/extract-variable/price.js
+++ ch6/src/extract-variable/price.js
@@ -3,10 +3,7 @@ export default function price(order) {
   const basePrice = order.quantity * order.itemPrice;
   const qunatityDiscount =
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
 
-  return (
-    basePrice -
-    qunatityDiscount +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  return basePrice - qunatityDiscount + shipping;
 }
