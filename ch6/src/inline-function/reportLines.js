9a6044eb5e8a511d823abe3b4802b3e479dd4f09
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-12 21:09:29 +0900

    [Chapter 6] Add example code for complicated inline function

---
+++ ch6/src/inline-function/reportLines.js
@@ -0,0 +1,10 @@
+export default function reportLines(aCustomer) {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+}
+
+function gatherCustomerData(out, aCustomer) {
+  out.push(["name", aCustomer.name]);
+  out.push(["location", aCustomer.location]);
+}
