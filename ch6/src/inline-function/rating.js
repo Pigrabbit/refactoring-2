918e72bcb222b5934300c13f802c19a9f8341152
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-12 21:02:45 +0900

    [Chapter 6] Add example code for inline function

---
+++ ch6/src/inline-function/rating.js
@@ -0,0 +1,7 @@
+export default function rating(aDriver) {
+  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
+}
+
+function moreThanFiveLateDeliveries(aDriver) {
+  return aDriver.numberOfLateDeliveries > 5;
+}
