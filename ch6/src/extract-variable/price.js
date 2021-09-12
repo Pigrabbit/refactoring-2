4e9d7f695149be84e1cd31dca77e7e3134a25d0e
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-12 21:37:26 +0900

    [Chapter 6] Extract basePrice variable

--- ch6/src/extract-variable/price.js
+++ ch6/src/extract-variable/price.js
@@ -1,7 +1,8 @@
 export default function price(order) {
   // price = base price - quantity discount + ship fee
+  const basePrice = order.quantity * order.itemPrice;
   return (
-    order.quantity * order.itemPrice -
+    basePrice -
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
     Math.min(order.quantity * order.itemPrice * 0.1, 100)
   );
