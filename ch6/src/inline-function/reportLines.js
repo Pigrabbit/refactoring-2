19b685887041a193d6de0ae540541535dadd9b23
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-12 21:11:22 +0900

    [Chapter 6] Inline gatherCustomerData function

--- ch6/src/inline-function/reportLines.js
+++ ch6/src/inline-function/reportLines.js
@@ -1,10 +1,7 @@
 export default function reportLines(aCustomer) {
   const lines = [];
-  gatherCustomerData(lines, aCustomer);
-  return lines;
-}
+  lines.push(["name", aCustomer.name]);
+  lines.push(["location", aCustomer.location]);
 
-function gatherCustomerData(out, aCustomer) {
-  out.push(["name", aCustomer.name]);
-  out.push(["location", aCustomer.location]);
+  return lines;
 }
