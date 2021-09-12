1af9f5b6144c550ce43fde3a436edbd4b10de4cd
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-12 21:38:35 +0900

    [Chapter 6] Extract quantityDiscount variable

--- ch6/src/extract-variable/price.js
+++ ch6/src/extract-variable/price.js
@@ -1,9 +1,12 @@
 export default function price(order) {
   // price = base price - quantity discount + ship fee
   const basePrice = order.quantity * order.itemPrice;
+  const qunatityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+
   return (
     basePrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    qunatityDiscount +
     Math.min(order.quantity * order.itemPrice * 0.1, 100)
   );
 }
