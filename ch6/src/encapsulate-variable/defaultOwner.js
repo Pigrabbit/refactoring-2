ca38cf7c03f8daeda246048746ee471aeefa516b
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-13 17:06:43 +0900

    [Chapter 6] Encapsulate Record

--- ch6/src/encapsulate-variable/defaultOwner.js
+++ ch6/src/encapsulate-variable/defaultOwner.js
@@ -1,9 +1,24 @@
 let defaultOwnerData = { firstName: "Martin", lastName: "Fowler" };
 
 export function defaultOwner() {
-  return { ...defaultOwnerData };
+  return new Person(defaultOwnerData);
 }
 
 export function setDefaultOwner(arg) {
   defaultOwnerData = arg;
+}
+
+class Person {
+  constructor({ firstName, lastName }) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
 }
