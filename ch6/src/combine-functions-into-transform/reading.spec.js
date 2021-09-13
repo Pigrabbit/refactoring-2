9c8629f711f1e4a5d7f2c7982251f0d63a5b644c
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-13 23:10:20 +0900

    [Chapter 6] Add test checks baseReading unchanged

--- ch6/src/combine-functions-into-transform/reading.spec.js
+++ ch6/src/combine-functions-into-transform/reading.spec.js
@@ -1,4 +1,9 @@
-import { getBaseCharge, getTaxableCharge } from "./reading.after";
+import { cloneDeep } from "lodash";
+import {
+  getBaseCharge,
+  getTaxableCharge,
+  enrichReading,
+} from "./reading.after";
 
 describe("getBaseCharge", () => {
   it("calculates base charge", () => {
@@ -9,5 +14,21 @@ describe("getBaseCharge", () => {
 describe("getTaxableCharge", () => {
   it("calculates taxable charge", () => {
     expect(getTaxableCharge()).toEqual(1);
+  });
+});
+
+describe("enrichReading", () => {
+  it("check reading unchanged", () => {
+    const baseReading = {
+      customer: "Ivan",
+      quantity: 15,
+      month: 5,
+      year: 2017,
+    };
+    const oracle = cloneDeep(baseReading);
+
+    enrichReading(baseReading);
+
+    expect(baseReading).toEqual(oracle);
   });
 });
