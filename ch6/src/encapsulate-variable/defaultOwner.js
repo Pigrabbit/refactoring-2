a18b3e704733f0626a0b7e66225e5a07c375c523
Donghyuk Kang <donghyuk1003+github@gmail.com>
2021-09-13 16:59:43 +0900

    [Chapter 6] Encapsulate defaultOwner variable

---
+++ ch6/src/encapsulate-variable/defaultOwner.js
@@ -0,0 +1,9 @@
+let defaultOwnerData = { firstName: "Martin", lastName: "Fowler" };
+
+export function defaultOwner() {
+  return defaultOwnerData;
+}
+
+export function setDefaultOwner(arg) {
+  defaultOwnerData = arg;
+}
--- ch6/src/encapsulate-variable/initSpaceship.js
+++ ch6/src/encapsulate-variable/initSpaceship.js
@@ -1,14 +1,13 @@
 import Spaceship from "./spaceShip";
-
-let defaultOwner = { firstName: "Martin", lastName: "Fowler" };
+import { defaultOwner, setDefaultOwner } from "./defaultOwner";
 
 export default function initSpaceship() {
   const spaceship1 = new Spaceship();
-  spaceship1.owner = defaultOwner;
+  spaceship1.owner = defaultOwner();
 
-  defaultOwner = { firstName: "Rebecca", lastName: "Parsons" };
+  setDefaultOwner({ firstName: "Rebecca", lastName: "Parsons" });
   const spaceship2 = new Spaceship();
-  spaceship2.owner = defaultOwner;
+  spaceship2.owner = defaultOwner();
 
   return [spaceship1, spaceship2];
 }
